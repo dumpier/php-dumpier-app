12e8b66c9deecc7b4d0271c3c236f43222be0a7c
noncense <noncensexnoncense@gmail.com>
2019-02-10 23:45:51 +0900

    #1 facade helper

--- facade.php
+++ facade.php
@@ -2,29 +2,29 @@
 /**
  * @property \App\Services\TestService $testService
  *
- * @property \App\Models\Repositories\Player\PlayerCharacterRepository $playerCharacterRepository
- * @property \App\Models\Repositories\Player\PlayerDeckRepository $playerDeckRepository
- * @property \App\Models\Repositories\Player\PlayerWeaponRepository $playerWeaponRepository
- * @property \App\Models\Repositories\PlayerManage\PlayerManageRepository $playerManageRepository
+ * @property \App\Models\Repositories\Player\PlayerCharacterRepository $playerCharacter
+ * @property \App\Models\Repositories\Player\PlayerDeckRepository $playerDeck
+ * @property \App\Models\Repositories\Player\PlayerWeaponRepository $playerWeapon
+ * @property \App\Models\Repositories\PlayerManage\PlayerManageRepository $playerManage
  */
 class Controller extends \Presto\Mvc\Controller {}
 
 /**
  * @property \App\Services\TestService $testService
  *
- * @property \App\Models\Repositories\Player\PlayerCharacterRepository $playerCharacterRepository
- * @property \App\Models\Repositories\Player\PlayerDeckRepository $playerDeckRepository
- * @property \App\Models\Repositories\Player\PlayerWeaponRepository $playerWeaponRepository
- * @property \App\Models\Repositories\PlayerManage\PlayerManageRepository $playerManageRepository
+ * @property \App\Models\Repositories\Player\PlayerCharacterRepository $playerCharacter
+ * @property \App\Models\Repositories\Player\PlayerDeckRepository $playerDeck
+ * @property \App\Models\Repositories\Player\PlayerWeaponRepository $playerWeapon
+ * @property \App\Models\Repositories\PlayerManage\PlayerManageRepository $playerManage
  */
 class Service extends \Presto\Service {}
 
 /**
  * @property \App\Services\TestService $testService
  *
- * @property \App\Models\Repositories\Player\PlayerCharacterRepository $playerCharacterRepository
- * @property \App\Models\Repositories\Player\PlayerDeckRepository $playerDeckRepository
- * @property \App\Models\Repositories\Player\PlayerWeaponRepository $playerWeaponRepository
- * @property \App\Models\Repositories\PlayerManage\PlayerManageRepository $playerManageRepository
+ * @property \App\Models\Repositories\Player\PlayerCharacterRepository $playerCharacter
+ * @property \App\Models\Repositories\Player\PlayerDeckRepository $playerDeck
+ * @property \App\Models\Repositories\Player\PlayerWeaponRepository $playerWeapon
+ * @property \App\Models\Repositories\PlayerManage\PlayerManageRepository $playerManage
  */
 class Command extends \Presto\Consoles\Command {}
