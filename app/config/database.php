77c72afd4a11c382c109a28f2c061603730127ed
noncense <noncensexnoncense@gmail.com>
2019-02-16 15:33:31 +0900

    #1 db list

--- app/config/database.php
+++ app/config/database.php
@@ -1,12 +1,14 @@
 <?php
 return [
-    'default'=>"admin",
+    "default"=>"admin",
 
     "connections"=>[
-        'admin'         =>['host'=>'127.0.0.1', 'port'=>3306, 'db'=>'thrones_admin', 'character'=>'utf8', 'user'=>'thrones', 'pass'=>'thrones', ],
+        "admin" =>["driver"=>"mysqli", "host"=>"presto.homestead.test", "port"=>3306, "db"=>"thrones_admin", "character"=>"utf8", "user"=>"thrones", "pass"=>"thrones", ],
 
-        'master'        =>['host'=>'127.0.0.1', 'port'=>3306, 'db'=>'thrones_master', 'character'=>'utf8', 'user'=>'thrones', 'pass'=>'thrones', ],
-        'player_manage' =>['host'=>'127.0.0.1', 'port'=>3306, 'db'=>'thrones_player_manage', 'character'=>'utf8', 'user'=>'thrones', 'pass'=>'thrones', ],
-        'player'        =>['host'=>'127.0.0.1', 'port'=>3306, 'db'=>'thrones_player', 'character'=>'utf8', 'user'=>'thrones', 'pass'=>'thrones', ],
+        "master" =>["driver"=>"mysqli", "host"=>"presto.homestead.test", "port"=>3306, "db"=>"thrones_master", "character"=>"utf8", "user"=>"thrones", "pass"=>"thrones", ],
+        "player" =>["driver"=>"mysqli", "host"=>"presto.homestead.test", "port"=>3306, "db"=>"thrones_player", "character"=>"utf8", "user"=>"thrones", "pass"=>"thrones", ],
+        "player_shard_01" =>["driver"=>"mysqli", "host"=>"presto.homestead.test", "port"=>3306, "db"=>"thrones_player_shard_01", "character"=>"utf8", "user"=>"thrones", "pass"=>"thrones", ],
+
+        "log_01" =>["driver"=>"mysqli", "host"=>"presto.homestead.test", "port"=>3306, "db"=>"thrones_log_01", "character"=>"utf8", "user"=>"thrones", "pass"=>"thrones", ],
     ],
 ];
