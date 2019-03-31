78708b79a3c58d155777ed4dee2d3f4f3a96d953
noncense <noncensexnoncense@gmail.com>
2019-03-31 22:18:29 +0900

    #2 [wip]アカウント作成

--- config/database.php
+++ config/database.php
@@ -3,12 +3,14 @@ return [
     "default"=>"admin",
 
     "connections"=>[
-        "admin" =>["driver"=>"mysqli", "host"=>"presto.homestead.test", "port"=>3306, "db"=>"thrones_admin", "character"=>"utf8", "user"=>"thrones", "pass"=>"thrones", ],
+        "admin" =>["driver"=>"mysqli", "host"=>"presto.homestead.test", "port"=>3306, "db"=>"presto_admin", "character"=>"utf8", "user"=>"thrones", "pass"=>"thrones", ],
 
-        "master" =>["driver"=>"mysqli", "host"=>"presto.homestead.test", "port"=>3306, "db"=>"thrones_master", "character"=>"utf8", "user"=>"thrones", "pass"=>"thrones", ],
-        "player" =>["driver"=>"mysqli", "host"=>"presto.homestead.test", "port"=>3306, "db"=>"thrones_player", "character"=>"utf8", "user"=>"thrones", "pass"=>"thrones", ],
-        "player_shard_01" =>["driver"=>"mysqli", "host"=>"presto.homestead.test", "port"=>3306, "db"=>"thrones_player_shard_01", "character"=>"utf8", "user"=>"thrones", "pass"=>"thrones", ],
+        "master" =>["driver"=>"mysqli", "host"=>"presto.homestead.test", "port"=>3306, "db"=>"presto_master", "character"=>"utf8", "user"=>"thrones", "pass"=>"thrones", ],
+        "player" =>["driver"=>"mysqli", "host"=>"presto.homestead.test", "port"=>3306, "db"=>"presto_player", "character"=>"utf8", "user"=>"thrones", "pass"=>"thrones", ],
+        "player_manage" =>["driver"=>"mysqli", "host"=>"presto.homestead.test", "port"=>3306, "db"=>"presto_player", "character"=>"utf8", "user"=>"thrones", "pass"=>"thrones", ],
 
-        "log_01" =>["driver"=>"mysqli", "host"=>"presto.homestead.test", "port"=>3306, "db"=>"thrones_log_01", "character"=>"utf8", "user"=>"thrones", "pass"=>"thrones", ],
+        "player_shard_01" =>["driver"=>"mysqli", "host"=>"presto.homestead.test", "port"=>3306, "db"=>"presto_player_shard_01", "character"=>"utf8", "user"=>"thrones", "pass"=>"thrones", ],
+
+        "log_01" =>["driver"=>"mysqli", "host"=>"presto.homestead.test", "port"=>3306, "db"=>"presto_log_01", "character"=>"utf8", "user"=>"thrones", "pass"=>"thrones", ],
     ],
 ];
