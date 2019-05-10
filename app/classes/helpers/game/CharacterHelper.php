e93f55b485ffd87ffe2ff037cccd87006dddf937
noncense <noncensexnoncense@gmail.com>
2019-05-11 06:42:28 +0900

    #2 [wip]battle画面周り

--- app/classes/helpers/game/CharacterHelper.php
+++ app/classes/helpers/game/CharacterHelper.php
@@ -9,35 +9,23 @@ class CharacterHelper
 
     public function image(int $character_id=0, int $size=50, int $border_size=0)
     {
-        $real_size = $size - $border_size * 2;
-        $background_size = $real_size * 5;
-        list($position_x, $position_y) = $this->getPosition($character_id, $real_size, $real_size);
-
-
-        return <<<EOF
-<div style="width: {$size}px; height:{$size}px; border: solid {$border_size}px #fd7e14; border-radius: 5px; ">
-  <div style="width:{$real_size}px; height:{$real_size}px;
-    background-image :url('/res/images/character-dummy/fase/2face-all.jpg');
-    background-repeat: no-repeat;
-    background-size: {$background_size}px;
-    background-position: -{$position_x}px -{$position_y}px;">
-  </div>
-</div>
-EOF;
+        return $this->size($character_id, $size, $border_size);
     }
 
     public function size(int $character_id=0, int $size=50, int $border_size=2)
     {
         $real_size = $size - $border_size * 2;
-        $background_size = $real_size * 5;
+        $background_size_x = $real_size * 4;
+        $background_size_y = $real_size * 2;
         list($position_x, $position_y) = $this->getPosition($character_id, $real_size, $real_size);
+        $url = $this->url($character_id);
 
         return <<<EOF
-<div style="width: {$size}px; height:{$size}px; border: solid {$border_size}px #fd7e14; border-radius: 5px; ">
+<div class="character-icon" style="width: {$size}px; height:{$size}px; border: solid {$border_size}px #fd7e14; border-radius:5px;  box-shadow:2px 2px 2px 2px rgba(0,0,0,0.2); ">
   <div style="width:{$real_size}px; height:{$real_size}px;
-    background-image :url('/res/images/character-dummy/fase/face-all.jpg');
+    background-image :url('{$url}');
     background-repeat: no-repeat;
-    background-size: {$background_size}px;
+    background-size: {$background_size_x}px {$background_size_y}px;
     background-position: -{$position_x}px -{$position_y}px;">
   </div>
 </div>
@@ -47,6 +35,7 @@ EOF;
 
     private function getPosition(int $character_id=0, int $size_x, int $size_y)
     {
+        return [0, 0];
         list($x, $y) = $this->getXY($character_id);
 
         return [$x * $size_x, $y * $size_y];
@@ -55,6 +44,7 @@ EOF;
 
     private function getXY(int $character_id=0)
     {
+        return [0, 0];
         $character_id = !empty($character_id) ? $character_id - 1 : $character_id;
         $x = ($character_id % 5);
         $y = floor($character_id / 5);
@@ -62,5 +52,11 @@ EOF;
         return [$x, $y];
     }
 
+    public function url(int $character_id=0)
+    {
+        // return "/res/images/character-dummy/fase/2face-all.jpg";
+        $number = sprintf("%03d", $character_id);
+        return "/res/images/character/face/96x96/yhvxl{$number}.png";
+    }
 
 }
