d76a23bbe730c70dd049874b332b232c21ba1936
noncense <noncensexnoncense@gmail.com>
2019-05-26 16:49:20 +0900

    #2 [wip]アニメーション

---
+++ app/classes/services/master/MasterService.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Services\Admin;
+
+
+class MasterService extends \Service
+{
+    protected $repositories = [
+    ];
+
+
+    public function all(string $table)
+    {
+
+    }
+}
